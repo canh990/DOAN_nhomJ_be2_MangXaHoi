b7f87db4b7499959bf8ea068cf3be241f5bba72a
NguyenDang <nguyenlehaidang2685@gmail.com>
2026-04-23 17:45:43 +0700

    Commit database

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema; 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,8 +19,7 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        //
-        Schema::defaultStringLength(191);
-    }
+{
+    Schema::defaultStringLength(191);
+}
 }
